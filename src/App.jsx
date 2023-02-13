667e4a43584626c5d1f40e35475a6e350e8db0fa
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-02-13 13:41:51 +0700

    feat: use framer-motion instead of gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
-import { useState, useEffect, useLayoutEffect, useRef, Suspense } from "react";
-import { gsap } from "gsap";
+import { useState, useEffect, Suspense } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { Landing, Contact, Work } from "./components";
 import useStore from "./store";
 
 function App() {
   const theme = useStore((state) => state.theme);
-  const [loading, setLoading] = useState(true);
-  const pageRef = useRef();
-  const textRef = useRef();
-  const [ctx, setCtx] = useState(gsap.context(() => {}));
-
-  useLayoutEffect(() => {
-    const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-
-    tl.fromTo(
-      pageRef.current,
-      { opacity: 0 },
-      { opacity: 1, duration: 0.5 }
-    ).fromTo(
-      textRef.current,
-      { opacity: 0, y: 80 },
-      { opacity: 1, y: 0, duration: 1 },
-      "-=0.5"
-    );
-
-    ctx.add("endLoading", () => {
-      gsap.to(pageRef.current, {
-        opacity: 0,
-        duration: 0.5,
-        onComplete: () => setLoading(false),
-      });
-    });
-
-    return () => {
-      tl.kill();
-      ctx.revert();
-    };
-  }, []);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      ctx.endLoading();
+      setIsLoading(false);
     }, 2000);
 
     return () => {
@@ -49,22 +18,23 @@ function App() {
   }, []);
 
   return (
-    <>
-      {loading ? (
-        <Suspense>
-          <div className={`loading loading__${theme}`} ref={pageRef}>
-            <div className="loading__text" ref={textRef}>
-              Welcome
-            </div>
-          </div>
-        </Suspense>
+    <AnimatePresence>
+      {isLoading ? (
+        <motion.div
+          className={`loading loading__${theme}`}
+          key="modal"
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <motion.div className="loading__text">Welcome</motion.div>
+        </motion.div>
       ) : (
         <div className={`App App--${theme}`}>
-          {loading ? (
-            <div className={`loading loading__${theme}`} ref={pageRef}>
-              <div className="loading__text" ref={textRef}>
-                Welcome
-              </div>
+          {isLoading ? (
+            <div className={`loading loading__${theme}`}>
+              <div className="loading__text">Welcome</div>
             </div>
           ) : null}
           <Landing />
@@ -72,7 +42,7 @@ function App() {
           <Contact />
         </div>
       )}
-    </>
+    </AnimatePresence>
   );
 }
 
