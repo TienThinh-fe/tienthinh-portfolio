30a2ed9479372e4aaa8d5e77656662b26d3757e7
TienThinh-fe <tienthinhhoang317@gmail.com>
2024-04-26 00:35:00 +0700

    feat: second section and horizontal scroll

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import './reset.css'
 import './style.css'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const sections = gsap.utils.toArray('.main section')
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.main',
+    pin: true,
+    scrub: 1,
+    // end: '+=3000',
+    // snap: 1 / (sections.length - 1),
+    markers: true, // TODO: remove on production
+  },
+})
